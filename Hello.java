58e752298b5c459337025401fa7627cf43f56167
himanshuspringpeople <72376874+himanshuspringpeople@users.noreply.github.com>
2021-07-20 12:13:38 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
+package com.hello.main;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+   //     System.out.println("Hello, World!"); 
+        LOGGER.log("hello");
     }
 }
